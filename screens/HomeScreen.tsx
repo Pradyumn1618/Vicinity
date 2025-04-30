14c53f022c0dcbc853206222a8dd8a6f0b7b3946
Bhavesh <muppallabhaveshchowdary@gmail.com>
2025-04-30 10:54:03 +0530

    PostScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -13,6 +13,7 @@ import messaging from '@react-native-firebase/messaging';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { requestLocationPermission, startLocationTracking,requestNotificationPermission } from '../helper/locationPermission';
 import { promptForEnableLocationIfNeeded } from 'react-native-android-location-enabler';
+import NavigationBar from '../components/NavigationBar';
 
 interface HomeScreenProps {
   navigation: NavigationProp<any>;
@@ -116,20 +117,6 @@ const HomeScreen = ({ navigation }: HomeScreenProps) => {
     }, [checkAuthentication])
   );
 
-  const handleLogout = async () => {
-    try {
-      await auth().signOut();
-      mmkv.delete('user');
-      mmkv.delete('geohash');
-      navigation.reset({
-        index: 0,
-        routes: [{ name: 'Login' }],
-      });
-    } catch (error) {
-      console.error("Failed to logout:", error);
-    }
-  }
-
   const sendNotification = async () => {
     const user = auth().currentUser;
     if (!user) return;
@@ -167,28 +154,7 @@ const HomeScreen = ({ navigation }: HomeScreenProps) => {
       <Text className="text-white text-xl">Home Screen</Text>
       <Button title="Send Noti" onPress={() => sendNotification()} color="#4F46E5" /> {/* Optional custom color for visibility */}
 
-      {/* Bottom Navigation Buttons */}
-      <View className="absolute bottom-5 left-5 right-5 flex-row justify-around bg-zinc-900 py-3 rounded-xl shadow-lg border border-zinc-800" >
-        <TouchableOpacity onPress={() => navigation.navigate('Events')} className="items-center">
-          <Ionicons name="newspaper-outline" size={24} color="white" />
-          <Text className="text-white text-xs mt-1">Events</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => navigation.navigate('Profile')} className="items-center">
-          <Ionicons name="person-outline" size={24} color="white" />
-          <Text className="text-white text-xs mt-1">Profile</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => navigation.navigate('Inbox')} className="items-center">
-          <Ionicons name="chatbubble-outline" size={24} color="white" />
-          <Text className="text-white text-xs mt-1">Chat</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleLogout} className="items-center">
-          <Ionicons name="log-out-outline" size={24} color="white" />
-          <Text className="text-white text-xs mt-1">Logout</Text>
-        </TouchableOpacity>
-      </View>
+     <NavigationBar/>
     </View>
   );
 };
